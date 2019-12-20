8fdf010cd92d1b430a4c69d8ce781b0828bb8ef4
Janne Salokoski <salokoski.janne@gmail.com>
2019-12-21 00:22:26 +0200

    Implement a mockup of the app

--- src/Mockup/Mockup.js
+++ src/Mockup/Mockup.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./Mockup.scss";
 
+require('typeface-courier-prime');
+require('typeface-source-code-pro');
+
 const Mockup = () => {
   return (
     <div class="wrapper">
@@ -8,9 +11,10 @@ const Mockup = () => {
         <h1 class="pageTitle">VR-tracking-tool</h1>
       </header>
       <main>
-        <div class="view-selector section">
+        <div class="view_selector section">
           <span class="label">View:</span>
-          <span class="view_option">Train</span>
+          <span class="view_option selected">Train</span>
+          <span>|</span>
           <span class="view_option">Station</span>
         </div>
         <div class="settings section">
@@ -25,26 +29,39 @@ const Mockup = () => {
             <span class="from"><a href="#">Joensuu</a></span>
             <span class="label">To:</span>
             <span class="from"><a href="#">Helsinki</a></span>
+            <span class="label">| Status:</span>
+            <span class="status">Moving</span>
+            <span class="late">[+2:13]</span>
           </div>
+          <br />
           <div class="stations">
+            <div class="station headers">
+              <span class="station_name">STATION</span>
+              <span class="departure_time">T.TIME</span>
+              <span class="actual_departure_time">R.TIME</span>
+              <span class="late">DIFF</span>
+            </div>
             <div class="station passed">
               <span class="station_name">Joensuu</span>
               <span class="departure_time">10:14:00</span>
               <span class="actual_departure_time">→ 10:15:31</span>
               <span class="late">[+1:31]</span>
             </div>
-            <div class="station passed">
+            <div class="station current">
               <span class="station_name">Kittilä</span>
               <span class="departure_time">10:20:00</span>
-              <span class="estimated_departure_time">→ ~10:22:13</span>
+              <span class="actual_departure_time">→ 10:22:13</span>
               <span class="late">[+2:13]</span>
+            </div>
+            <div class="station coming">
+              <span class="station_name">Helsinki</span>
+              <span class="departure_time">10:40:00</span>
+              <span class="estimated_departure_time">→ ~10:44:27</span>
+              <span class="late">[+4:27]</span>
             </div>
           </div>
         </div>
       </main>
-      <footer>
-        <span class="copyright">Janne Salokoski &copy; 2019</span>
-      </footer>
     </div>
   );
 };
