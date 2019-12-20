f162e4ee5650e7d41e0eb6483bd06c3b8856c4ab
Janne Salokoski <salokoski.janne@gmail.com>
2019-12-21 00:25:08 +0200

    Remove mockup from loading at all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import Mockup from "./Mockup/Mockup.js";
-
 function VrTrackingTool() {
   return (
     <div className="vr-tracking-tool">
-      
+
     </div>
   );
 }
